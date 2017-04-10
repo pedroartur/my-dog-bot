3070c4a094da8f61862ef344a70ef214bcc6132c
pedroartur <pedroartur@hotmail.com>
2017-04-10 14:49:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Pedro? Just type "HELLO" to get started.')
+            return bot.say('So you want to learn about Pedro? Just type HELLO to get started.')
                 .then(() => 'speak');
         }
     },
